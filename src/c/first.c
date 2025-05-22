9b2ee92dd8d78a11311a2be9451b4a29d944a4a4
fish <innaias@harveyosity.org>
2025-05-22 00:10:03 -0700

    vertex normal shenanigans + monkey

--- src/c/first.c
+++ src/c/first.c
@@ -1,50 +1,130 @@
-#include "pebble_fonts.h"
 #include <pebble.h>
+#include <stdint.h>
 #include <stdio.h>
+#include <string.h>
 
+#include "gcolor_definitions.h"
+#include "model.h"
 #include "stronger_math.h"
-
-vec3 *accel_vec;
+#include "trig.h"
 
 static Window *s_main_window;
-static TextLayer *s_time_layer;
+static Layer *dots_layer;
 
-static void convert_accel_data(AccelData *pebble, vec3 *dvector) {
-  dvector->x = pebble->x / 1000.0f;
-  dvector->y = pebble->y / 1000.0f;
-  dvector->z = pebble->z / 1000.0f;
+#define yes true
+#define no false
+
+#define NELEMS(x) (sizeof(x) / sizeof((x)[0]))
+
+#define DOTS_COUNT VERTICES_LENGTH
+
+static vec3 accel_data;
+
+static basis3 basis;
+
+char *vec3_str(vec3 v) {
+  static char string[16];
+  snprintf(string, sizeof(string), "%03d, %03d, %03d", (int)(v.x * 100),
+           (int)(v.y * 100), (int)(v.z * 100));
+  return string;
 }
 
-static void main_window_unload(Window *window) {
-  text_layer_destroy(s_time_layer);
+char *basis_str(basis3 b) {
+  static char string[48];
+  char *x = vec3_str(b.x);
+  char *y = vec3_str(b.y);
+  char *z = vec3_str(b.z);
+  snprintf(string, sizeof(string), "%s, %s, %s", x, y, z);
+  // free(x);
+  // free(y);
+  // free(z);
+  return string;
 }
 
-static void redraw_text() {
-  // Get a tm structure
-  time_t temp = time(NULL);
-  struct tm *tick_time = localtime(&temp);
-
-  // Write the current hours and minutes into a buffer
-  static char s_buffer[32];
-  // strftime(s_buffer, sizeof(s_buffer), clock_is_24h_style() ?
-  // "%H:%M" : "%I:%M", tick_time);
-  snprintf(s_buffer, sizeof(s_buffer), "%d, %d, %d",
-           (int)(accel_vec->x * 10),
-           (int)(accel_vec->y * 10),
-           (int)(accel_vec->z * 10));
-
-  // Display this time on the TextLayer
-  text_layer_set_text(s_time_layer, s_buffer);
+static vec3 convert_accel_data(AccelData pebble) {
+  return VEC3(pebble.x / 1000.0, pebble.y / 1000.0, pebble.z / 1000.0);
 }
+
+static void main_window_unload(Window *window) { layer_destroy(dots_layer); }
 
 static void tick_handler(struct tm *tick_time, TimeUnits units_changed) {
   // redraw_text();
 }
 
 static void accel_data_handler(AccelData *data, uint32_t count) {
-  convert_accel_data(data, accel_vec);
-  *accel_vec = vec3Normalize(*accel_vec);
-  redraw_text();
+  vec3 new_accel_data = convert_accel_data(*data);
+  accel_data =
+      add_v3(accel_data, mulf_v3(sub_v3(new_accel_data, accel_data), 0.3));
+  basis =
+      orthonormalize_b3(rotate_b3(rotate_b3(basis, VEC3_Z, -accel_data.x * 0.1),
+                                  VEC3_X, -accel_data.y * 0.1));
+  // vec3 accel_vec = normalize_v3(accel_data);
+
+  // get an orthonormalized basis
+  // basis.z = accel_vec;
+  // basis.x = normalize_v3(cross(VEC3_X, basis.z));
+  // basis.y = cross(basis.x, basis.z);
+
+  // char *bs = basis_str(basis);
+  // APP_LOG(APP_LOG_LEVEL_DEBUG, "%s", bs);
+  // free(bs);
+}
+
+static GPoint convert_vec2(float x, float y, GRect bounds) {
+  int16_t min_dimension;
+  if (bounds.size.w < bounds.size.h) {
+    min_dimension = bounds.size.w;
+  } else {
+    min_dimension = bounds.size.h;
+  }
+
+  return GPoint((x * min_dimension + bounds.size.w) / 2,
+                (y * min_dimension + bounds.size.h) / 2);
+}
+
+static GPoint points[DOTS_COUNT];
+static uint16_t points_sizes[DOTS_COUNT];
+
+static void dots_update_handler(Layer *layer, GContext *ctx) {
+  GRect layer_frame = layer_get_bounds(layer);
+
+  // vec3 dots[DOTS_COUNT];
+
+  // for (size_t i = 0; i < DOTS_COUNT; i++) {
+  //   float y = 1 - (i / (float)(DOTS_COUNT - 1)) * 2;
+  //   float theta = PHI * i;
+  //   float radius = sqrt_approx(1.0 - y * y);
+  //   vertices[i] = VEC3(pebble_cos(theta) * radius, y, pebble_sin(theta) *
+  //   radius);
+  // }
+
+  for (size_t i = 0; i < DOTS_COUNT; i++) {
+    Vertex v = vertices[i];
+    vec3 xformed =
+        add_v3(basis_xform_3(v.position, basis), mulf_v3(VEC3_Y, 0.75));
+    vec3 xformed_normal = basis_xform_3(v.normal, basis);
+    // if (xformed_normal.y > 0) {
+    //   points_sizes[i] = -1;
+    //   continue;
+    // }
+    float recip_depth = 1.0 / xformed.y;
+    points[i] = convert_vec2(xformed.x * recip_depth, xformed.z * recip_depth,
+                             layer_frame);
+    points_sizes[i] = (uint16_t)(recip_depth * -xformed_normal.y + 1);
+  }
+
+  graphics_context_set_antialiased(ctx, no);
+  graphics_context_set_fill_color(ctx, GColorWhite);
+  graphics_context_set_compositing_mode(ctx, GCompOpAssign);
+  for (size_t i = 0; i < DOTS_COUNT; i++) {
+    if (points_sizes[i] > 0)
+      graphics_fill_circle(ctx, points[i], points_sizes[i]);
+  }
+}
+
+static void redraw_timer_handler(void *data) {
+  layer_mark_dirty(dots_layer);
+  app_timer_register(100, redraw_timer_handler, NULL);
 }
 
 static void main_window_load(Window *window) {
@@ -52,30 +132,26 @@ static void main_window_load(Window *window) {
   Layer *window_layer = window_get_root_layer(window);
   GRect bounds = layer_get_bounds(window_layer);
 
-  // Create the TextLayer with specific bounds
-  s_time_layer =
-      text_layer_create(GRect(0, PBL_IF_ROUND_ELSE(58, 52), bounds.size.w, 50));
-
-  // Improve the layout to be more like a watchface
-  text_layer_set_background_color(s_time_layer, GColorClear);
-  text_layer_set_text_color(s_time_layer, GColorBlack);
-  text_layer_set_text(s_time_layer, "00:00");
-  text_layer_set_font(s_time_layer, fonts_get_system_font(FONT_KEY_GOTHIC_18));
-  text_layer_set_text_alignment(s_time_layer, GTextAlignmentCenter);
+  dots_layer = layer_create(bounds);
 
   // Add it as a child layer to the Window's root layer
-  layer_add_child(window_layer, text_layer_get_layer(s_time_layer));
+  layer_add_child(window_layer, dots_layer);
 
-  accel_vec = (vec3 *)malloc(sizeof(vec3));
   AccelData *pebble_accel = (AccelData *)malloc(sizeof(AccelData));
   accel_service_peek(pebble_accel);
-  convert_accel_data(pebble_accel, accel_vec);
+  accel_data = convert_accel_data(*pebble_accel);
+  free(pebble_accel);
+
   accel_data_service_subscribe(1, accel_data_handler);
+
+  redraw_timer_handler(NULL);
 }
 
 static void init() {
+  basis = BASIS3_IDENTITY;
   // Create main Window element and assign to pointer
   s_main_window = window_create();
+  window_set_background_color(s_main_window, GColorBlack);
 
   // Set handlers to manage the elements inside the Window
   window_set_window_handlers(
@@ -84,13 +160,13 @@ static void init() {
 
   // Show the Window on the watch, with animated=true
   window_stack_push(s_main_window, true);
+  layer_set_update_proc(dots_layer, dots_update_handler);
   tick_timer_service_subscribe(MINUTE_UNIT, tick_handler);
+
+  APP_LOG(APP_LOG_LEVEL_DEBUG, "%d", sizeof(vertices));
 }
 
-static void deinit() {
-  window_destroy(s_main_window);
-  free(accel_vec);
-}
+static void deinit() { window_destroy(s_main_window); }
 
 int main(void) {
   init();
--- src/c/stronger_math.h
+++ src/c/stronger_math.h
@@ -1,5 +1,8 @@
 #pragma once
-#include <pebble.h>
+
+#include <stdint.h>
+
+#include "trig.h"
 
 typedef struct {
   float x;
@@ -7,8 +10,42 @@ typedef struct {
   float z;
 } vec3;
 
-#define VEC3(x, y, z)                                                          \
-  (vec3) { x, y, z }
+typedef struct {
+  vec3 x;
+  vec3 y;
+  vec3 z;
+} basis3;
+
+#define VEC3(x, y, z) ((vec3){x, y, z})
+
+#define VEC3_X VEC3(1.0, 0.0, 0.0)
+#define VEC3_Y VEC3(0.0, 1.0, 0.0)
+#define VEC3_Z VEC3(0.0, 0.0, 1.0)
+
+#define BASIS3_IDENTITY ((basis3){VEC3_X, VEC3_Y, VEC3_Z})
+
+float sqrt_approx(float z) {
+  union {
+    float f;
+    uint32_t i;
+  } val = {z}; /* Convert type, preserving bit pattern */
+  /*
+   * To justify the following code, prove that
+   *
+   * ((((val.i / 2^m) - b) / 2) + b) * 2^m = ((val.i - 2^m) / 2) + ((b + 1) / 2)
+   * * 2^m)
+   *
+   * where
+   *
+   * b = exponent bias
+   * m = number of mantissa bits
+   */
+  val.i -= 1 << 23; /* Subtract 2^m. */
+  val.i >>= 1;      /* Divide by 2. */
+  val.i += 1 << 29; /* Add ((b + 1) / 2) * 2^m. */
+
+  return val.f; /* Interpret again as float */
+}
 
 float inv_sqrt(float x) {
   float xhalf = 0.5f * x;
@@ -23,23 +60,77 @@ float inv_sqrt(float x) {
   return u.x;
 }
 
-float dot(vec3 a, vec3 b) { return a.x * b.x + a.y * b.y + a.z * b.z; }
+float dot_v3(vec3 a, vec3 b) { return a.x * b.x + a.y * b.y + a.z * b.z; }
 
-float length_sqr(vec3 v) { return dot(v, v); }
+float length_sqr_v3(vec3 v) { return dot_v3(v, v); }
 
-float inv_length(vec3 x) { return inv_sqrt(length_sqr(x)); }
+float inv_length_v3(vec3 x) { return inv_sqrt(length_sqr_v3(x)); }
 
-vec3 multiply(vec3 a, float b) { return VEC3(a.x * b, a.y * b, a.z * b); }
+vec3 mulf_v3(vec3 a, float b) { return VEC3(a.x * b, a.y * b, a.z * b); }
 
-vec3 divide(vec3 a, float b) { return VEC3(a.x / b, a.y / b, a.z / b); }
+vec3 divf_v3(vec3 a, float b) { return VEC3(a.x / b, a.y / b, a.z / b); }
 
-vec3 normalize(vec3 x) { return multiply(x, inv_length(x)); }
+vec3 addf_v3(vec3 a, float b) { return VEC3(a.x + b, a.y + b, a.z + b); }
+
+vec3 subf_v3(vec3 a, float b) { return VEC3(a.x - b, a.y - b, a.z - b); }
+
+vec3 mul_v3(vec3 a, vec3 b) { return VEC3(a.x * b.x, a.y * b.y, a.z * b.z); }
+
+vec3 div_v3(vec3 a, vec3 b) { return VEC3(a.x / b.x, a.y / b.y, a.z / b.z); }
+
+vec3 add_v3(vec3 a, vec3 b) { return VEC3(a.x + b.x, a.y + b.y, a.z + b.z); }
+
+vec3 sub_v3(vec3 a, vec3 b) { return VEC3(a.x - b.x, a.y - b.y, a.z - b.z); }
+
+vec3 normalize_v3(vec3 x) { return mulf_v3(x, inv_length_v3(x)); }
 
 vec3 cross(vec3 a, vec3 b) {
   return VEC3(a.y * b.z - a.z * b.y, a.z * b.x - a.x * b.z,
               a.x * b.y - a.y * b.x);
 }
 
-vec3 basis_xform(vec3 a, vec3 x, vec3 y, vec3 z) {
-  return VEC3(dot(a, x), dot(a, y), dot(a, z));
+basis3 basis3_transpose(basis3 basis) {
+  return (basis3){.x = VEC3(basis.x.x, basis.y.x, basis.z.x),
+                  .y = VEC3(basis.x.y, basis.y.y, basis.z.y),
+                  .z = VEC3(basis.x.z, basis.y.z, basis.z.z)};
+}
+
+vec3 basis_xform_3(vec3 a, basis3 basis) {
+  basis3 t_basis = basis3_transpose(basis);
+  return VEC3(dot_v3(a, t_basis.x), dot_v3(a, t_basis.y), dot_v3(a, t_basis.z));
+}
+
+vec3 rotate_v3(vec3 v, vec3 axis, float angle) {
+  // v * cos(angle) + (axis x v) * sin(angle) + axis * (axis . v) * (1 -
+  // cos(angle))
+  float cos = pebble_cos(angle);
+  float sin = pebble_sin(angle);
+  return add_v3(add_v3(mulf_v3(v, cos), mulf_v3(cross(axis, v), sin)),
+                mulf_v3(axis, dot_v3(axis, v) * (1.0 - cos)));
+}
+
+basis3 rotate_b3(basis3 basis, vec3 axis, float angle) {
+  return (basis3){rotate_v3(basis.x, axis, angle),
+                  rotate_v3(basis.y, axis, angle),
+                  rotate_v3(basis.z, axis, angle)};
+}
+
+basis3 normalize_b3(basis3 basis) {
+  return (basis3){
+      normalize_v3(basis.x),
+      normalize_v3(basis.y),
+      normalize_v3(basis.z),
+  };
+}
+
+basis3 orthonormalize_b3(basis3 basis) {
+  vec3 x = basis.x;
+  vec3 y = basis.y;
+  vec3 z = basis.z;
+  x = normalize_v3(x);
+  y = sub_v3(y, mulf_v3(x, dot_v3(x, y)));
+  y = normalize_v3(y);
+  z = sub_v3(sub_v3(z, mulf_v3(x, dot_v3(x, z))), mulf_v3(y, dot_v3(y, z)));
+  z = normalize_v3(z);
+  return (basis3) { x, y, z };
 }
